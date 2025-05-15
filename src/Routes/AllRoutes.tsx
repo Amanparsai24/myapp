af1015211b3b92cdc848bd64f11e8094574e33b5
DESKTOP-1T8TJVP\10_3 <aman@sphinx-solution.com>
2025-05-15 16:45:33 +0530

    UPDATES

--- src/Routes/AllRoutes.tsx
+++ src/Routes/AllRoutes.tsx
@@ -1,7 +1,9 @@
 import { lazy } from 'react';
 
+
 const Login = lazy(() => import('../pages/Login'));
 const Dashboard = lazy(() => import('../pages/authpages/Dashboard'));
+const User = lazy(() => import('../pages/User/Userlist'));
 
 const publicRoutes = [
     { path: "/", component: <Login /> },
@@ -12,6 +14,8 @@ const publicRoutes = [
 
 const authProtectedRoutes = [
     { path: "/dashboard", component: <Dashboard /> },
+    { path: "/user", component: <User /> },
+
 ];
 
 export { publicRoutes, authProtectedRoutes };
